9065daa02426f88022afa42e864cef2fb08cbaa4
Your Name <marissuss@gmail.com>
2024-01-23 14:29:11 +0200

    inital files and language setup

---
+++ app/Http/Middleware/LanguagePrefixMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LanguagePrefixMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        $language = $request->segment(1);
+    
+        if (in_array($language, config('app.locales'))) {
+            app()->setLocale($language);
+        } else {
+            abort(Response::HTTP_NOT_FOUND);
+        }
+
+        return $next($request);
+    }
+}
---
+++ lang/lv/welcome.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    // Headers
+    'Documentation header' => 'Dokumentācija',
+    'Laracasts header' => 'Larakasts',
+    'Laravel News header' => 'Laravel Jaunumi',
+    'Vibrant Ecosystem header' => 'Dzīvīga Ekosistēma',
+
+    // Body
+    'Documantation' => 'Sveika pasaule! Laravel ir brīnišķīga dokumentācija, kas aptver katru aspektu ietvaru. Vai esat jaunpienācējs vai jums ir iepriekšēja pieredze ar Laravel, mēs iesakām izlasīt mūsu dokumentāciju no sākuma līdz beigām.',
+    'Laracasts' => 'Laracasts piedāvā tūkstošiem video pamācību par Laravel, PHP un JavaScript izstrādi. Pārbaudiet tos, pārliecinieties pats un masveidā paaugstiniet savas izstrādes prasmes procesā.',
+    'Laravel News' => 'Laravel News ir kopienas portāls un jaunumu apkopojums, kas apkopo visus jaunākos un svarīgākos jaunumus Laravel ekosistēmā, ieskaitot jaunu pakalpojumu izlaišanu un pamācības.',
+    'Vibrant Ecosystem' => 'Laravel spēcīgā pirmās puses rīku un bibliotēku bibliotēka, piemēram, Forge, Vapor, Nova un Envoyer, palīdz jums pāriet uz nākamo līmeni. Savienojiet tos ar spēcīgām atvērtā koda bibliotēkām, piemēram, Cashier, Dusk, Echo, Horizon, Sanctum, Telescope un citiem.'
+];
